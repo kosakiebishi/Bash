9daa50038c2fffd119c39ea67e3b4721cb26a774
kosakiebishi <greg.alias.kozak@gmail.com>
2015-04-05 01:09:31 +0200

    Update and rename .gitignore to dualMonitors.sh

---
+++ dualMonitors.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+	list=`xrandr -q |fmt -1 |grep -B 1 -E '^connected' |sed 's/connected//g' |sed 's/--//g' |tr -s "\n" " "`
+
+	i=0;for item in $list;do
+		array[$i]=$item
+		i=$(( $i + 1 ))
+	done
+
+	if [ "${array[1]}" = "" ];then
+		echo "There is only one source"
+		zenity --warning --height=20 --text="There is only one source of video signal \n - $array"
+	else
+		zenity --warning --height=20 --text="Sources:  \n $list"
+ 
+		while [ 1 = 1 ]; do
+			choose=`zenity --height=250 --width=400  \
+			--list \
+				--text="Co chcesz zrobic? \n\n" \
+			--radiolist \
+				--title='Selection' \
+				--column=Boxes \
+				--column=Selections TRUE "DualView" FALSE "Monitor-2-Off" FALSE "Monitor-2-On"  \
+				--separator=':'`
+			
+			if [ "$choose" = "DualView" ];then
+				xrandr --auto --output ${array[0]} --left-of ${array[1]}
+			elif [ "$choose" = "Monitor-2-Off" ];then
+				xrandr --output ${array[1]} --off
+			elif [ "$choose" = "Monitor-2-On" ];then
+				xrandr --output ${array[1]} --auto
+			else
+				echo "exit"
+				exit
+			fi
+		done
+	fi
